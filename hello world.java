0dab8f9de7a78eaccda1e5aec43c43f0c48dd0b4
Kirill Bersenev <kirillfox2000@mail.ru>
2021-09-16 14:22:39 +0300

    commit2

--- hello world.java
+++ hello world.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
     }
 }
